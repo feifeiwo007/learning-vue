f4dd593c66a86397d344d247d82679d22ce5f7ac
wu fei <wufei@MacBook-Pro.local>
2020-07-25 16:50:22 +0800

    aa

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learning-vue
+* 用vue的框架的话 main.js 是入口
+* 在html原来data是一个对象,在vue里面变成了一个方法，返回一个对象，因为 app.vue 已经不是根实例了,根实例在main.js 里面的new vue 里面
---
+++ vue-deno/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+    message: "hello world",
+    list: [
+      {
+        title: "class1",
+        del: false,
+      },
+      {
+        title: "class2",
+        del: true,
+      },
+    ],
+  }},
+  methods: {
+    handledelete(val){
+      console.log('handledelete',val)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
